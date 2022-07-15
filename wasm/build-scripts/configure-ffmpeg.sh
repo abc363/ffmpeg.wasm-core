fad3dd8dfc0483128a2aa82aba965ba9a0f0d58e
rosiefu <rosiefu@tencent.com>
2022-07-15 11:29:40 +0800

    feat: 修改配置

--- wasm/build-scripts/configure-ffmpeg.sh
+++ wasm/build-scripts/configure-ffmpeg.sh
@@ -5,23 +5,42 @@ source $(dirname $0)/var.sh
 
 FLAGS=(
   "${FFMPEG_CONFIG_FLAGS_BASE[@]}"
-  --enable-gpl            # required by x264
-  --enable-nonfree        # required by fdk-aac
-  --enable-zlib           # enable zlib
-  --enable-libx264        # enable x264
-  --enable-libx265        # enable x265
-  --enable-libvpx         # enable libvpx / webm
-  --enable-libwavpack     # enable libwavpack
-  --enable-libmp3lame     # enable libmp3lame
-  --enable-libfdk-aac     # enable libfdk-aac
-  --enable-libtheora      # enable libtheora
-  --enable-libvorbis      # enable libvorbis
-  --enable-libfreetype    # enable freetype
-  --enable-libopus        # enable opus
-  --enable-libwebp        # enable libwebp
-  --enable-libass         # enable libass
-  --enable-libfribidi     # enable libfribidi
-  # --enable-libaom         # enable libaom
+  --target-os=none        # use none to prevent any os specific configurations
+  --arch=x86_32           # use x86_32 to achieve minimal architectural optimization
+  --enable-cross-compile  # enable cross compile
+  --disable-x86asm        # disable x86 asm
+  --disable-inline-asm    # disable inline asm
+  --disable-stripping     # disable stripping
+  --disable-programs      # disable programs build (incl. ffplay, ffprobe & ffmpeg)
+  --disable-doc           # disable doc
+  --nm="llvm-nm"
+  --ar=emar
+  --ranlib=emranlib
+  --cc=emcc
+  --cxx=em++
+  --objcc=emcc
+  --dep-cc=emcc
+  # 去掉不需要的库
+  --disable-avdevice
+  --disable-swresample
+  --disable-postproc
+  --disable-network
+  --disable-pthreads
+  --disable-w32threads
+  --disable-os2threads
+  # 配置需要的解封装，编解码器等
+  --disable-everything # 减少wasm体积的关键，除了以下的组件外的个别组件都disable
+  --enable-filters
+  --enable-muxer=image2
+  --enable-demuxer=mov # mov,mp4,m4a,3gp,3g2,mj2
+  --enable-demuxer=flv
+  --enable-demuxer=h264
+  --enable-demuxer=asf
+  --enable-encoder=mjpeg
+  --enable-decoder=hevc
+  --enable-decoder=h264
+  --enable-decoder=mpeg4
+  --enable-protocol=file
 )
 echo "FFMPEG_CONFIG_FLAGS=${FLAGS[@]}"
 emconfigure ./configure "${FLAGS[@]}"
